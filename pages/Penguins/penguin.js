0bc76d5c99674749a63fabbc206a8e170497b9d9
HeyYeh <ian-bousher@hotmail.com>
2017-12-24 17:13:48 +0000

    Penguin

---
+++ pages/Penguins/penguin.js
@@ -0,0 +1,71 @@
+$("#pitch").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#pitchContent").offset().top-130},
+        'slow');
+});
+
+$("#WDTG").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#WDTGContent").offset().top-130},
+        'slow');
+});
+
+$("#DYAF").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#DYAFContent").offset().top-130},
+        'slow');
+});
+
+$("#TSS").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#TSSContent").offset().top-130},
+        'slow');
+});
+
+$("#ZPO").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#ZPOContent").offset().top-130},
+        'slow');
+});
+
+$("#scripts").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#scriptsContent").offset().top-130},
+        'slow');
+});
+
+$("#IAGASU").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#IAGASUContent").offset().top-130},
+        'slow');
+});
+
+$("#ITMS").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#ITMSContent").offset().top-130},
+        'slow');
+});
+
+$("#ideas").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#ideasContent").offset().top-130},
+        'slow');
+});
+
+$("#socMed").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#socMedContent").offset().top-130},
+        'slow');
+});
+
+$("#concepts").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#conceptsContent").offset().top-130},
+        'slow');
+});
+
+$("#misc").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#miscContent").offset().top-130},
+        'slow');
+});
